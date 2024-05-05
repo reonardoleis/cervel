5d1cfe777ffc7700fde85005a515eae4c927cfcf
Leonardo <accs.lndrs@gmail.com>
2024-05-05 17:34:41 -0300

    update to last version

---
+++ deploy_home.sh
@@ -0,0 +1,12 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+cd ~/projects/me
+
+git pull origin main
+
+npm install
+npm run build
+
+cp -r ~/projects/me/dist/* ~/projects/frontend_deployments
